bccf9531e5644fe68df602cd415c1884940d413d
JoshHunter11 <weirdstuff1105@gmail.com>
2022-03-09 23:01:09 +0000

    new platform placement

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -37,12 +37,12 @@
         createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
 
         // example:
-        createPlatform(400, 500, 0.25);
-        createPlatform(600, 375, 0.33);
-        createPlatform(150, 125, 0.33);
-        createPlatform(400, 250, 0.25);
-        createPlatform(150, 375, 0.33);
-        createPlatform(600, 125, 0.33);
+        createPlatform(400, 400, 0.25);
+        createPlatform(600, 320, 0.33);
+        createPlatform(150, 150, 0.33);
+        createPlatform(400, 200, 0.25);
+        createPlatform(150, 320, 0.33);
+        createPlatform(600, 150, 0.33);
 
         createPlatform(150, 575, 0.5);
         
