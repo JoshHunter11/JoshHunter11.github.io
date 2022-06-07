1a3265cf336d6bccd336742e04d30ed0326bfbc3
JoshHunter11 <weirdstuff1105@gmail.com>
2022-06-07 20:59:49 +0000

    first day of the code

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -1,8 +1,8 @@
 /* global $, sessionStorage */
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
-function runProgram(){
+
+function runProgram() {
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -10,9 +10,20 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+
+  var speedX = 0;
+  var speedY = 0;
+  var positionX = 0;
+  var positionY = 0;
+
   // Game Item Objects
 
+  var KEY = {
+    "UP": 38,
+    "DOWN": 40,
+    "LEFT": 37,
+    "RIGHT": 39
+  }
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
@@ -27,22 +38,41 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    repositionGameItem();
+    redrawGameItem();
   }
-  
+
   /* 
   Called in response to events.
   */
   function handleKeyDown(event) {
-      console.log(event.key);
+    if (event.which === 38) {
+      console.log("up arrow pressed");
+    }
+    else if (event.which === 40) {
+      console.log("down arrow pressed");
+    }
+    else if (event.which === 37) {
+      console.log("left arrow pressed");
+    }
+    else if (event.which === 39) {
+      console.log("right arrow pressed");
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  
+  function repositionGameItem(){
+    walker.css('left', newPositionX);
+    walker.css('top', newPositionY);
+  }
+
+  function redrawGameItem(){
+
+  }
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
@@ -50,5 +80,5 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
-  
+
 }
