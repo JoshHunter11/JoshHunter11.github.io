3af56b0355f5b69ddd5051fa10bac9799b5417c6
JoshHunter11 <weirdstuff1105@gmail.com>
2022-06-13 01:51:21 +0000

    starting on TODO 7

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -1,6 +1,6 @@
 // This is a small program. There are only two sections. This first section is what runs
 // as soon as the page loads and is where you should call your functions.
-$(document).ready(function(){
+$(document).ready(function () {
     const $display = $('#display');
 
     // Multiple TODOs: Call your apply function(s) here
@@ -17,17 +17,19 @@ $(document).ready(function(){
 /////////////////////////////////////////////////////////
 
 applyFilter(reddify);
+applyFilterNoBackground(decreaseBlue);
+applyFilterNoBackground(increaseGreenByBlue);
 //applyAndRender();
 
 
 // TODO 1, 2 & 4: Create the applyFilter function here
 
-function applyFilter(filterFunction){
-    for(var i = 0; i < image.length; i++){
+function applyFilter(filterFunction) {
+    for (var i = 0; i < image.length; i++) {
 
         var row = image[i];
 
-        for(var j = 0; j < row.length; j++){
+        for (var j = 0; j < row.length; j++) {
 
             //takes each pixel of the image
             var rgbString = image[i][j];
@@ -47,34 +49,69 @@ function applyFilter(filterFunction){
 
 // TODO 7: Create the applyFilterNoBackground function
 
+function applyFilterNoBackground(filterFunction) {
 
-// TODO 5: Create the keepInBounds function
+    var backgroundColor = image[0];
 
+    for (var i = 0; i < image.length; i++) {
 
+        var row = image[i];
 
-function keepInBounds(num){
+        if (image[i][j] === backgroundColor) {
 
-    function setMin (x){
-        var minimum = Math.max(x, 0);
-        return minimum;
+            for (var j = 0; j < row.length; j++) {
+
+                //takes each pixel of the image
+                var rgbString = image[i][j];
+
+                //changes each pixel of the array (currently all strings), and changes them into arrays
+                var rgbNumbers = rgbStringToArray(rgbString);
+
+                //changes the red value of each pixel, now an array, to the max
+                filterFunction(rgbNumbers);
+
+                //turns the new array pixel values back into strings
+                image[i][j] = rgbArrayToString(rgbNumbers);
+
+            }
+
+        }else{}
     }
-  
-    function setMax (minimum){
-        var max = Math.min(minimum, 255);
-        return max;
-    }
-    
-    return setMax(setMin(num));
 
 }
 
-// TODO 3: Create reddify function
+    // TODO 5: Create the keepInBounds function
 
-function reddify(arr){
-    arr[RED] = 200;
-}
+    function keepInBounds(num) {
 
-// TODO 6: Create more filter functions
+        function setMin(x) {
+            var minimum = Math.max(x, 0);
+            return minimum;
+        }
 
+        function setMax(minimum) {
+            var max = Math.min(minimum, 255);
+            return max;
+        }
+
+        return setMax(setMin(num));
+
+    }
+
+    // TODO 3: Create reddify function
+
+    function reddify(arr) {
+        arr[RED] = 200;
+    }
+
+    // TODO 6: Create more filter functions
+
+    function decreaseBlue(arrPt2) {
+        arrPt2[BLUE] = keepInBounds(arrPt2[BLUE] - 50);
+    }
+
+    function increaseGreenByBlue(arrPt3) {
+        arrPt3[GREEN] = keepInBounds(arrPt3[BLUE] + arrPt3[GREEN]);
+    }
 
 // CHALLENGE code goes below here
