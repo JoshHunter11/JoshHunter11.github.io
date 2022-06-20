6cd053da6d7382ba2054256480b0656c8a3a9c5a
JoshHunter11 <weirdstuff1105@gmail.com>
2022-06-20 20:58:08 +0000

    getting things moving and completing the planning section

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -11,9 +11,12 @@ function runProgram() {
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
 
+  const BOARD_WIDTH = $("#board").width();
+  const BOARD_HEIGHT = $("#board").height();
+
   // Game Item Objects
 
-  var rightPaddle = Paddle('#rightPaddle', 380);
+  var rightPaddle = Paddle('#rightPaddle', 740);
   console.log(rightPaddle);
 
 
@@ -22,8 +25,10 @@ function runProgram() {
 
   var ball = {};
   ball.id = "#ball"
-  ball.x = 210;
-  ball.y = 210;
+  ball.x = 385;
+  ball.y = 385; 
+  ball.width = 20;
+  ball.height = 20;
   ball.speedX = 0;
   ball.speedY = 0;
 
@@ -31,7 +36,7 @@ function runProgram() {
     var obj = {};
     obj.id = id;
     obj.x = x;
-    obj.y = 175;
+    obj.y = 345;
     obj.speedY = 0;
     obj.speedX = 0;
     obj.width = $(id).width();
@@ -54,6 +59,7 @@ function runProgram() {
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
   $(document).on('keyup', handleKeyUp);
+  startBall();
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -64,8 +70,13 @@ function runProgram() {
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    moveObject();
+    moveObject(ball);
+    moveObject(leftPaddle);
+    moveObject(rightPaddle);
 
+    wallCollision(ball);
+    wallCollision(rightPaddle);
+    wallCollision(leftPaddle);
   }
 
   /* 
@@ -75,22 +86,22 @@ function runProgram() {
 
     if (event.which === KEY.UP1) {
       console.log("w key pressed");
-      leftPaddle.speedY = 5;
+      leftPaddle.speedY = -5;
     }
 
     else if (event.which === KEY.UP2) {
       console.log("up arrow key pressed");
-      rightPaddle.speedY = 5;
+      rightPaddle.speedY = -5;
     }
 
     else if (event.which === KEY.DOWN1) {
       console.log("s key pressed");
-      leftPaddle.speedY = -5;
+      leftPaddle.speedY = 5;
     }
 
     else if (event.which === KEY.DOWN2) {
       console.log("down arrow key pressed");
-      rightPaddle.speedY = -5;
+      rightPaddle.speedY = 5;
     }
 
   }
@@ -133,19 +144,49 @@ function runProgram() {
   }
 
   function startBall(){
-    ball.x = 210;
-    ball.y = 210;
+    ball.x = 385;
+    ball.y = 385;
     ball.speedX = (Math.random() * 3 + 2) * (Math.random() > 0.5 ? -1 : 1);
     ball.speedY = (Math.random() * 3 + 2) * (Math.random() > 0.5 ? -1 : 1);
   }
 
-  function moveObject(object){
-    object.x += object.speedX;
-    object.y += object.speedY;
+  function moveObject(obj){
+    obj.x += obj.speedX;
+    obj.y += obj.speedY;
 
-    $(object.id).css('left', object.x);
-    $(object.id).css('top', object.y);
+    $(obj.id).css('left', obj.x);
+    $(obj.id).css('top', obj.y);
 
+  }
+
+  function wallCollision(obj){
+
+    if(obj.x >= BOARD_WIDTH - obj.width){
+      obj.speedX = 0;
+      obj.speedY = 0;
+      obj.x = BOARD_WIDTH - obj.width;
+    }
+    
+    else if(obj.x <= 0){
+      obj.speedX = 0;
+      obj.speedY = 0;
+    }
+    else if(obj.y < 0){
+      obj.speedY = 0;
+      obj.y = 0;
+    }
+    else if(obj.y >= BOARD_HEIGHT - obj.height){
+      obj.speedY = 0;
+      obj.y = BOARD_HEIGHT - obj.height;
+    }
+
+  }
+
+  function scoreUpdate(ball){
+    if(ball.x >= BOARD_WIDTH - ball.width){
+      $("#scoreId").text(updatedScore);
+      startBall();
+    }
   }
 
 }
