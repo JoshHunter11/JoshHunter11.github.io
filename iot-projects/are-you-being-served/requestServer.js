fb579f59c66834076d5e8038461f269c40f84bf0
JoshHunter11 <weirdstuff1105@gmail.com>
2022-09-29 19:00:33 -0500

    starting are you being served

--- iot-projects/are-you-being-served/requestServer.js
+++ iot-projects/are-you-being-served/requestServer.js
@@ -1,1 +1,11 @@
 // requestServer.js file
+const http = require('http');
+const request = require('request');
+const port = 8686;
+
+http.createServer(function(req, res){
+    res.writeHead(404, {'content-Type': 'text/plain'});
+    res.write("Here's the entire bee movie script: ");
+    res.write("wait ");
+    res.end('nvm it doesnt work');
+}).listen(port);
