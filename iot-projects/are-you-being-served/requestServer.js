58aa63f417a4a4a28a9174cefa1fafabf1b0d5ab
JoshHunter11 <weirdstuff1105@gmail.com>
2022-10-04 18:59:27 -0500

    working on are you being served

--- iot-projects/are-you-being-served/requestServer.js
+++ iot-projects/are-you-being-served/requestServer.js
@@ -1,11 +1,31 @@
 // requestServer.js file
-const http = require('http');
-const request = require('request');
+const http = require("http");
+const request = require("request");
 const port = 8686;
 
-http.createServer(function(req, res){
-    res.writeHead(404, {'content-Type': 'text/plain'});
-    res.write("Here's the entire bee movie script: ");
-    res.write("wait ");
-    res.end('nvm it doesnt work');
-}).listen(port);
+http.createServer(requestListenerFunction).listen(port);
+
+function requestListenerFunction(req, res) {
+  request("https://JoshHunter11.github.io", callbackFunction);
+}
+
+function callbackFunction(error, response, body) {
+  if (!body || !response || (error === null && response.statusCode !== 200)) {
+    res.end("bad URL\n");
+    return response.statusCode;
+  }
+
+  if (response.statusCode === 200 && !error === true) {
+    res.statusCode = 200;
+    res.writeHead("text/html");
+    res.write(body);
+  } 
+  
+  else if (response.statusCode !== 200 || !error === false) {
+    res.statusCode = response.statusCode;
+    res.writeHead("text/html");
+    res.write(error.toString());
+  }
+
+  res.end();
+}
