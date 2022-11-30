3f3a413febb9b7bfd6374a0d9c84a4df77270386
JoshHunter11 <weirdstuff1105@gmail.com>
2022-11-29 18:28:41 -0600

    checking todo 4

--- iot-projects/first-slice-of-pi/routes/actuators.js
+++ iot-projects/first-slice-of-pi/routes/actuators.js
@@ -1,7 +1,25 @@
-const express = require('express'),
-	router = express.Router(),
-	resources = require('./../resources/model');
+const express = require("express"),
+  router = express.Router(),
+  resources = require("./../resources/model");
 
-	
+router.route("/").get(function (req, res, next) {
+  res.send(resources.pi.actuators);
+});
+
+router.route("/leds").get(function (req, res, next) {
+  res.send(resources.pi.actuators.leds);
+});
+
+router.route('/leds/:id').get(function (req, res, next) {
+    res.send(resources.pi.actuators.leds[req.params.id]);
+});
+
+// router.route("/leds/1").get(function (req, res, next) {
+//   res.send(resources.pi.sensors);
+// });
+
+// router.route("/leds/2").get(function (req, res, next) {
+//   res.send(resources.pi.sensors);
+// });
 
 module.exports = router;
