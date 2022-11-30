8c0b3c7270d43ef80ae715f591a580720cb3426d
JoshHunter11 <weirdstuff1105@gmail.com>
2022-11-29 18:15:31 -0600

    checking todo 3

--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -1,7 +1,11 @@
+var sensorRoutes = require('./../routes/sensors');
+
 const express = require("express"),
   cors = require("cors");
 var app = express();
 app.use(cors());
+
+app.use('/pi/sensors', sensorRoutes);
 
 app.get("/", function (req, res) {
   res.send("Someone has pinged the home page!");
