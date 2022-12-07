db90f53dec12ea5c0b5d546e1fd33bc6948a7b99
JoshHunter11 <weirdstuff1105@gmail.com>
2022-12-06 18:55:53 -0600

    starting pi in your face

--- iot-projects/pi-inyourface/plugins/internal/ledsPlugin.js
+++ iot-projects/pi-inyourface/plugins/internal/ledsPlugin.js
@@ -1,12 +1,42 @@
-const resources = require('./../../resources/model');
+const resources = require("./../../resources/model");
+const Gpio = require("onoff").Gpio;
 
 let actuator1, actuator2;
 let model = resources.pi.actuators.leds;
-let pluginName = resources.pi.actuators.leds[1].name + ", " + resources.pi.actuators.leds[2].name;
+let pluginName =
+  resources.pi.actuators.leds[1].name +
+  ", " +
+  resources.pi.actuators.leds[2].name;
 
 exports.start = function (params) {
-	connectHardware();
-	console.log("starting " + pluginName + " plugin");
+  connectHardware();
+  console.log("starting " + pluginName + " plugin");
 };
 
 // TODO 1: Complete the ledsPlugin!
+
+function connectHardware() {
+  sensor.watch(function (err, val) {});
+  actuator1 = new Gpio(device.gpio, "out");
+  actuator2 = new Gpio(device.gpio, "out");
+}
+
+function stop() {
+  actuator1.write(0);
+  actuator2.write(0);
+
+  actuator1.unexport();
+  actuator2.unexport();
+}
+
+exports.switchOnOff = {
+  1: function (value) {
+    // turn LED 1 on or off based on value
+	if(actuator1.value === true){
+		
+	}
+  },
+  2: function (value) {
+    // turn LED 2 on or off based on value
+  },
+};
